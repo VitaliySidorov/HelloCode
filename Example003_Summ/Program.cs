c6874230d72d02443902b2a9f04d67d1da8f1a56
Vitaliy <vitaliysidorov@yandex.ru>
2023-01-12 21:57:37 +0500

    Change string for WriteLine

--- Example003_Summ/Program.cs
+++ Example003_Summ/Program.cs
@@ -3,8 +3,4 @@ int numberB = new Random().Next(1, 10);
 
 int result = numberA + numberB;
 
-Console.Write(numberA);
-Console.Write(" + ");
-Console.Write(numberB);
-Console.Write(" = ");
-Console.WriteLine(result);
+Console.WriteLine(numberA + " + " + numberB + " = " + result);
